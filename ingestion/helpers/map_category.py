aa69699a8173dca4e704a8b9d0df62799d957658
hungnv1302 <hunn1302@gmail.com>
2026-04-05 21:39:57 +0700

    delete old ingestion

--- ingestion/helpers/map_category.py
+++
@@ -1,8 +0,0 @@
-def map_category(category: str)->str:
-  mapping = {
-    'hoctap_vanphong': 'Học tập / Văn phòng',
-    'gaming': 'Gaming', 
-    'dohoa': 'Đồ họa',
-    'laptrinh': 'Lập trình'
-  }
-  return mapping.get(category.lower(), category)
